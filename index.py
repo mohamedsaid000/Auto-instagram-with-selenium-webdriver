aed46ddb06e7502ddc994b44f504eea9861373ba
mohamedsaid000 <mohamed.said.ai098@gmail.com>
2021-01-01 04:21:19 -0600

    Update index.py

--- index.py
+++ index.py
@@ -5,42 +5,47 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+#open chrome and open instagram site
 driver.get("https://www.instagram.com/")
 time.sleep(3)
 
+#login with the username ans pass
 username = driver.find_element_by_name("username")
 username.send_keys("username")
 password = driver.find_element_by_name("password")
 password.send_keys("password")
 
+#click login
 login = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
 login.click()
 
+#sleep for 5 seconds, let the page load
 time.sleep(5)
 
-
+#continue
 turn_on = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[1]')
 turn_on.click()
 
+#let the browser load
 time.sleep(3)
 
 
 
 
-#create an array that will generate numbers
+#creating an array than contains numbers
 Number = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30']
 
 def follow():
     
     driver.get("https://www.instagram.com/explore/people/suggested/")
     time.sleep(2)
-    #follow people
+    
 
     for id in Number:
         #time.sleep(1)
         follow = driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div[2]/div/div/div["+id+"]/div[3]/button")
         follow.click()
-        
+        #follow people
     time.sleep(3)
 
 follow()    
@@ -57,5 +62,6 @@ def unfollow():
         
         follow = driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[1]")
         follow.click()
+        #unfollow people
 #unfollow()
 
